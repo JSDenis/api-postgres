02bfd1137a8a8b0ef9ecea62bef9153e5dd303af
JSDenis <32075762+JSDenis@users.noreply.github.com>
2022-06-27 17:01:08 +0300

    update db.js

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ const pool = new Pool({
     password: 'a7276dac9b69f0ee0194d1b5e37e623accc7d10a505dc81ad3e5bd1447df60a0',
     host:'ec2-44-205-41-76.compute-1.amazonaws.com',
     port: 5432,
-    database: 'd1bhltp53osta4'
+    database: 'd1bhltp53osta4',
+    ssl: {rejectUnauthorized: false}
 })
 
 module.exports = pool
